3ae80f3ee35fc2329918a0dd6ad8b9c7f2b48b07
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2026-07-22 12:31:40 +0900

    Style | 10_LiveScene에 P_UI_Live 캔버스 프리팹 배치 및 씬 저장

--- Assets/02. Scripts/02-02. UI/02-02-10. Live/UI_Live.cs
+++ Assets/02. Scripts/02-02. UI/02-02-10. Live/UI_Live.cs
@@ -8,12 +8,12 @@ namespace VELO.UI
         [Foldout("Sub UI Panels")]
         [SerializeField] private UI_LiveScorePanel _scorePanel;
         [SerializeField] private UI_LiveComboPanel _comboPanel;
-        [SerializeField] private UI_LiveHitPanel _hitPanel;
+        [SerializeField] private UI_LiveHitPanel _judgementPanel;
         [SerializeField] private UI_TrapezoidLanes _noteLanes;
 
         public UI_LiveScorePanel ScorePanel => _scorePanel;
         public UI_LiveComboPanel ComboPanel => _comboPanel;
-        public UI_LiveHitPanel HitPanel => _hitPanel;
+        public UI_LiveHitPanel JudgementPanel => _judgementPanel;
         public UI_TrapezoidLanes NoteLanes => _noteLanes;
     }
 }
